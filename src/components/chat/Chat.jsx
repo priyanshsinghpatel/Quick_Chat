8212eee9356e939999e3b83a6f1d28d92eb30143
priyanshsinghpatel <patelpsp206@gmail.com>
2025-01-16 11:54:23 +0530

    Emoji Picker

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react";
 
 
 const Chat = () => {
+ const [open,setOpen] = useState(false);
+ const [text,setText] = useState("");
+
+ const handleEmoji = e => {
+  setText((prev) => prev + e.emoji);
+  setOpen(false)
+};
+
+
   return (
     <div className='chat'>
       <div className="top">
@@ -19,7 +30,24 @@ const Chat = () => {
         </div>
       </div>
       <div className="center"></div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className="icons">          
+        <img src="./img.png" alt="" />
+        <img src="./camera.png" alt="" />
+        <img src="./mic.png" alt="" />
+        </div>
+        <input type="text"
+         placeholder="Type a message...."
+         value={text}
+         onChange={(e) => setText(e.target.value)} />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" onClick={()=>setOpen(prev=>!prev)} />         
+        </div>
+        <div className="picker">
+        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
     </div>
   )
 }
